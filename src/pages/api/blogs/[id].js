8d135e9b4febc627ee3b72884397a6404c43194a
Ahmad <scriptwithahmad@gmail.com>
2024-09-18 18:53:47 +0500

    update & delete api done

--- src/pages/api/blogs/[id].js
+++ src/pages/api/blogs/[id].js
@@ -1,5 +1,6 @@
 import blogsModel from "@/models/blogs";
 import dbConnect from "@/config/dbConnect";
+import { trackDynamicDataAccessed } from "next/dist/server/app-render/dynamic-rendering";
 
 export default async function handler(req, res) {
   dbConnect();
@@ -29,9 +30,61 @@ export default async function handler(req, res) {
         console.log(error);
       }
 
-      
+    case "DELETE":
+
+
+      try {
+
+        // console.log(req.query.id)
+
+        const delBlog = await blogsModel.findByIdAndDelete(req.query.id);
+
+        if (!delBlog) {
+          return res.status(200).json({
+            status: false,
+            message: "Blog Not found",
+          });
+        }
+
+        return res.status(200).json({
+          status: true,
+          message: "Blog Deleted successfully",
+        });
+
+      } catch (error) {
+        console.log(error);
+      }
 
       break;
+
+    case "PUT":
+
+      try {
+
+        const updateBlog = await blogsModel.findByIdAndUpdate(req.query.id, { ...req.body }, { new: true })
+
+
+        // if no blog found
+        if (!updateBlog) {
+          return res.status(404).json({
+            success: false,
+            message: "blog Not found!",
+          });
+        }
+
+        return res.status(200).json({
+          status: true,
+          message: "Blog Updated succesfully 😍",
+          updateBlog,
+        });
+
+
+      } catch (error) {
+        console.log(error)
+      }
+
+      break;
+
 
     default:
       break;
