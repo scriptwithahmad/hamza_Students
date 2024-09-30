6aee4c0ff553efe9bb44a09ebee9ad8cac17349d
Ahmad <scriptwithahmad@gmail.com>
2024-09-30 18:26:14 +0500

    url change for db

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        DB_URI: "mongodb+srv://ahmed:ahmed@edify.9anuaq1.mongodb.net/hamza_Stu_project?retryWrites=true&w=majority&appName=edify",
+        DB_URI: "add_your_mongodb_uri_here",
 
 
         // Cloudinary ------------------------------
